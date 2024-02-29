ab409a7c1df3befb6579bea28ce143c791c83e9d
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-02-29 23:09:21 +0100

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProjektLAB
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsValid()
+        {
+            if(RegisterLoginTxt.Text.Length < 4) 
+            {
+                MessageBox.Show("Wypełnij Pole Loginu w rejestracji - użyj minimum 4 znaków!", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(RegisterPasswordTxt.Password.Length < 5) 
+            {
+                MessageBox.Show("Wypełnij Pole Hasła w rejestracji - użyj minimum 5 znaków!", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(First_NameTxt.Text.Length == 0) 
+            {
+                MessageBox.Show("Wypełnij Pole Imienia w rejestracji", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(Last_NameTxt.Text.Length == 0)
+            {
+                MessageBox.Show("Wypełnij Pole Nazwiska w rejestracji", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            if(BirthDayDP.Text.Length == 0) 
+            {
+                MessageBox.Show("Wypełnij Pole Daty Urodzin w rejestracji", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                return false;
+            }
+            return true;
+        }
+
+        private void RegisterBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if(IsValid()) 
+            {
+                User registerUser = new User()
+                {
+                    Login = RegisterLoginTxt.Text,
+                    Password = RegisterPasswordTxt.Password,
+                    First_Name = First_NameTxt.Text,
+                    Last_Name = Last_NameTxt.Text,
+                    BirthDay = BirthDayDP.Text
+                };
+                UserServiceDataBase.AddNewUserToDataBase(registerUser);
+            }
+           else
+            {
+                return;
+            }
+        }
+
+
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ UserServiceDataBase.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ProjektLAB
+{
+    public static class UserServiceDataBase
+    {
+        public static string connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+        public static void AddNewUserToDataBase(User user)
+        {
+            if (user == null) 
+            {
+                MessageBox.Show("Błąd przekazania użytkownika do bazy danych!", "Błąd krytyczny bazy danych", MessageBoxButton.OKCancel, MessageBoxImage.Error);
+                return;
+            }
+            else
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    string query = @"INSERT INTO [dbo].[User] (Login, Password, First_Name, Last_Name, BirthDay) VALUES (@Login, @Password, @First_Name, @Last_Name, @BirthDay)";
+
+                    using(SqlCommand cmd = new SqlCommand(query, conn)) 
+                    {
+                        cmd.Parameters.AddWithValue("@Login", user.Login);
+                        cmd.Parameters.AddWithValue("@Password", user.Password);
+                        cmd.Parameters.AddWithValue("@First_Name", user.First_Name);
+                        cmd.Parameters.AddWithValue("@Last_Name", user.Last_Name);
+                        cmd.Parameters.AddWithValue("@BirthDay", user.BirthDay);
+
+                        try
+                        {
+                            conn.Open();
+                            int result = cmd.ExecuteNonQuery();
+
+                            if(result > 0) 
+                            {
+                                MessageBox.Show("Zostałeś pomyślnie zarejestrowany - nastąpi automatyczne przekierowanie do aplikacji po kliknięciu OK", "Pomyślna Rejestracja!", MessageBoxButton.OK, MessageBoxImage.Information);
+                            }
+                            else
+                            {
+                                MessageBox.Show("Nie udało się dodać użytkownika do bazy danych.", "Błąd rejestracji", MessageBoxButton.OK, MessageBoxImage.Error);
+                            }
+                        }
+                        catch (SqlException ex)
+                        {
+                            MessageBox.Show($"Wystąpił błąd podczas dodawania użytkownika do bazy danych: {ex.Message}", "Błąd krytyczny bazy danych", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                        catch(Exception ex) 
+                        {
+                            MessageBox.Show($"Wystąpił niespodziewany błąd podczas dodawania użytkownika do bazy danych: {ex.Message}", "Błąd krytyczny bazy danych", MessageBoxButton.OK, MessageBoxImage.Error);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
