8eb27380db0d3cd1464990586adf526fd85caa99
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-03-05 21:46:46 +0100

    Navigation system added, few buttons in navigation added

--- TrainService/TrainServiceWindow.xaml.cs
+++ TrainService/TrainServiceWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ProjektLAB.TrainService.Pages;
 
 namespace ProjektLAB.TrainService
 {
@@ -19,7 +20,13 @@ namespace ProjektLAB.TrainService
     /// </summary>
     public partial class TrainServiceWindow : Window
     {
-        public User ? User { get; set; }
+        public User? User { get; set; }
+        private Dictionary<string, Page> PageDictionary = new Dictionary<string, Page>()
+{
+            { "Home", new HomePage()},
+            //Add new Page
+    };
+
         public TrainServiceWindow(User loggedUser)
         {
             InitializeComponent();
@@ -30,6 +37,15 @@ namespace ProjektLAB.TrainService
         {
             InitializeComponent();
             User = null;
+            //InitializeGuestLogin();
+        }
+
+        private void InitializeGuestLogin()
+        {
+            Schedule.IsEnabled = false;
+            Stations.IsEnabled = false;
+            Schedule.Background = new SolidColorBrush(Colors.Gray);
+            Stations.Background = new SolidColorBrush(Colors.Gray);
         }
 
         private void Border_MouseDown(object sender, MouseButtonEventArgs e)
@@ -37,9 +53,21 @@ namespace ProjektLAB.TrainService
             this.DragMove();
         }
 
-        private void ExitBtn_Click(object sender, RoutedEventArgs e) 
+        private void ExitBtn_Click(object sender, RoutedEventArgs e)
         {
             this.Close();
+        }
+
+        private void NavigationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button && button.Tag is string pageKey)
+            {
+                Page pageToNavigate;
+                if (PageDictionary.TryGetValue(pageKey, out pageToNavigate))
+                 { 
+                    ContentFrame.Navigate(pageToNavigate);
+                }
+            }
         }
     }
 }
