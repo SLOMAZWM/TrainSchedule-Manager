e4134bd988d15a9d20ae189735a2e96d3cfe67c1
Marcin Słomka <m.slomka@student.po.edu.pl>
2024-03-25 13:26:25 +0100

    Edit DetailWindow (RouteDetails) - Frontend

--- HashingHelper.cs
+++ HashingHelper.cs
@@ -14,10 +14,8 @@ namespace ProjektLAB
         {
             using (SHA256 sha256Hash = SHA256.Create())
             {
-                // ComputeHash - returns byte array  
                 byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));
 
-                // Convert byte array to a string   
                 StringBuilder builder = new StringBuilder();
                 for (int i = 0; i < bytes.Length; i++)
                 {
